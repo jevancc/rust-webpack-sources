4a4ed4784418d6488e359a2ee59ea4b82fcd629d
Jevan Chan <jevan.cnchan@gmail.com>
2018-06-08 10:25:09 +0800

    Fix pretty command

--- test-js/source-list-map/fixtures/from-to-tests/babel-source.js
+++ test-js/source-list-map/fixtures/from-to-tests/babel-source.js
@@ -4,34 +4,36 @@ var nums = evens.map((v, i) => v + i);
 
 // Statement bodies
 nums.forEach(v => {
-    if (v % 5 === 0) fives.push(v);
+  if (v % 5 === 0)
+    fives.push(v);
 });
 
 // Lexical this
 var bob = {
-    _name: "Bob",
-    _friends: [],
-    printFriends() {
-        this._friends.forEach(f => console.log(this._name + " knows " + f));
-    }
-};
+  _name: "Bob",
+  _friends: [],
+  printFriends() {
+    this._friends.forEach(f =>
+      console.log(this._name + " knows " + f));
+  }
+}
 
 class SkinnedMesh extends THREE.Mesh {
-    constructor(geometry, materials) {
-        super(geometry, materials);
+  constructor(geometry, materials) {
+    super(geometry, materials);
 
-        this.idMatrix = SkinnedMesh.defaultMatrix();
-        this.bones = [];
-        this.boneMatrices = [];
-        //...
-    }
-    update(camera) {
-        //...
-        super.update();
-    }
-    static defaultMatrix() {
-        return new THREE.Matrix4();
-    }
+    this.idMatrix = SkinnedMesh.defaultMatrix();
+    this.bones = [];
+    this.boneMatrices = [];
+    //...
+  }
+  update(camera) {
+    //...
+    super.update();
+  }
+  static defaultMatrix() {
+    return new THREE.Matrix4();
+  }
 }
 
 var obj = {
@@ -41,22 +43,23 @@ var obj = {
     handler,
     // Methods
     toString() {
-        // Super calls
-        return "d " + super.toString();
+     // Super calls
+     return "d " + super.toString();
     },
     // Computed (dynamic) property names
-    ["prop_" + (() => 42)()]: 42
+    [ "prop_" + (() => 42)() ]: 42
 };
 
 // Basic literal string creation
-`In JavaScript "\n" is a line-feed.`// Multiline strings
+`In JavaScript "\n" is a line-feed.`
+
+// Multiline strings
 `In JavaScript this is
- not legal.`;
+ not legal.`
 
 // Interpolate variable bindings
-var name = "Bob",
-    time = "today";
-`Hello ${name}, how are you ${time}?`;
+var name = "Bob", time = "today";
+`Hello ${name}, how are you ${time}?`
 
 // Construct an HTTP request prefix is used to interpret the replacements and construction
 GET`http://foo.org/bar?a=${a}&b=${b}
@@ -66,24 +69,21 @@ GET`http://foo.org/bar?a=${a}&b=${b}
       "bar": ${bar}}`(myOnReadyStateChangeHandler);
 
 // list matching
-var [a, , b] = [1, 2, 3];
+var [a, , b] = [1,2,3];
 
 // object matching
-var {
-    op: a,
-    lhs: { op: b },
-    rhs: c
-} = getASTNode();
+var { op: a, lhs: { op: b }, rhs: c }
+       = getASTNode()
 
 // object matching shorthand
 // binds `op`, `lhs` and `rhs` in scope
-var { op, lhs, rhs } = getASTNode();
+var {op, lhs, rhs} = getASTNode()
 
 // Can be used in parameter position
-function g({ name: x }) {
-    console.log(x);
+function g({name: x}) {
+  console.log(x);
 }
-g({ name: 5 });
+g({name: 5})
 
 // Fail-soft destructuring
 var [a] = [];
