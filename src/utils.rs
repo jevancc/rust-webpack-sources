1474a1ed06d288d0fcbc2700f3185a34b3e99040
Jevan Chan <jevan.cnchan@gmail.com>
2018-07-16 23:31:42 +0800

    fast string split for single byte UTF8 chars

--- src/utils.rs
+++ src/utils.rs
@@ -1,14 +1,20 @@
+use std::cmp;
+
 #[inline]
 pub fn split_str(s: &str, pos: i32, single_byte_char_only: bool) -> (&str, &str, bool, bool) {
     if pos <= 0 {
         ("", s, true, single_byte_char_only)
     } else {
         let s_bytes = s.len();
-        let split_pos = s
-            .char_indices()
+        let split_pos = if single_byte_char_only {
+            cmp::min(pos as usize, s_bytes)
+        } else {
+            s.char_indices()
             .skip(pos as usize)
             .next()
-            .map_or(s_bytes, |(p, _)| p);
+            .map_or(s_bytes, |(p, _)| p)
+        };
+
         let (ls, rs) = s.split_at(split_pos);
         (ls, rs, split_pos == pos as usize, single_byte_char_only)
     }
